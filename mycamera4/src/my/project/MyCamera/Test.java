78e829d62d007da4529df1daf527f43777c320a5
BB <bb@b.om>
2017-02-12 15:54:02 +0100

    -

---
+++ mycamera4/src/my/project/MyCamera/Test.java
@@ -0,0 +1,16 @@
+package my.project.MyCamera;
+
+import java.net.*;
+import java.util.*;
+import java.util.regex.*;
+import java.io.*;
+
+public class Test {
+
+    
+    public static void main(String[] args) {
+
+	System.out.println(""+Calendar.getInstance().getTimeInMillis() );
+
+    }
+}
