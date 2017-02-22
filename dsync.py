d3d74291b73befc117c1ebf82fc6bc870a5e06f0
BB <bb@b.om>
2017-02-22 13:46:02 +0100

    -

--- dsync.py
+++ dsync.py
@@ -13,9 +13,9 @@ if sys.argv[1] == "kitaplar":
     os.system("python rsync.py /media/burak/6A4D-5BF0/kitaplar '/media/burak/New Volume/archive/kitaplar' --delete")
 
 if sys.argv[1] == "flash":
-    os.system("python rsync.py '/home/burak/Documents/bass' '/media/burak/UUI/backup/bass' --delete")
-    os.system("python rsync.py '/home/burak/Documents/kod' '/media/burak/UUI/backup/kod' --delete")
-    os.system("python rsync.py '/home/burak/Documents/classnotes' '/media/burak/UUI/backup/classnotes' --delete")
+    os.system("python rsync.py '/home/burak/Documents/bass' '/media/burak/7502-42E6/bass' --delete")
+    os.system("python rsync.py '/home/burak/Documents/kod' '/media/burak/7502-42E6/kod' --delete")
+    os.system("python rsync.py '/home/burak/Documents/classnotes' '/media/burak/7502-42E6/classnotes' --delete")
     
 if sys.argv[1] == "hd2hd":
     os.system("python rsync.py '/media/burak/New Volume/archive/bass' '/media/burak/17EA-3758/archive/bass' --delete")
