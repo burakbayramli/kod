cd4f9ce1b7cf3a7e8147209feea63970956d2f5a
BB <bb@b.om>
2017-03-07 09:01:33 +0100

    -

--- guide/celeb.py
+++ guide/celeb.py
@@ -1,12 +1,12 @@
 from datetime import datetime
 import pandas as pd, mindmeld
 
-fin = open('./data/famousbday.txt')
+fin = open('/home/burak/Documents/out.txt')
 for line in fin:
     try:
         toks = line.strip().split(":")
         dd =  datetime.strptime(toks[2], '%d/%m/%Y').date()
         ddd = dd.strftime('%Y%m%d')
         res =  mindmeld.calculate(ddd)['millman']
-        if res[0] == 31 and res[1] == 4: print toks[0], toks[2]
+        if res[0] == 38 and res[1] == 11: print toks[0], toks[2]
     except: pass
