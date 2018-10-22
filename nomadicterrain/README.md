0ef4a3aa6c7b7b985cc8857b7e0189e371ab10d7
Burak Bayramli <burak@localhost.com>
2018-10-22 21:26:04 +0200

    -

---
+++ nomadicterrain/README.md
@@ -0,0 +1,6 @@
+
+Some tools that might come in handy to run on smartphone (Android)
+through Termux.
+
+
+
---
+++ nomadicterrain/map/test_map.py
@@ -0,0 +1,16 @@
+
+import plot_map
+pts = np.array([[42.657889, 18.087956],[42.674881, 18.144730]])
+plot_map.plot(pts,'out.png')
+
+
+
+
+
+
+
+
+
+
+
+
