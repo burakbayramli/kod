7ce80847bebf201a18ce6a9ca13545944c9b05ce
Burak Bayramli <burak@localhost.com>
2018-10-22 21:40:29 +0200

    -

--- nomadicterrain/map/test_map.py
+++ nomadicterrain/map/test_map.py
@@ -1,5 +1,8 @@
-
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 import plot_map
+
 pts = np.array([[42.657889, 18.087956],[42.674881, 18.144730]])
 plot_map.plot(pts,'out.png')
 
