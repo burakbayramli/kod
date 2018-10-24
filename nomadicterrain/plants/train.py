fa49cea35b9f5dedf52f349826186998e2206ac9
Burak Bayramli <burak@localhost.com>
2018-10-24 20:11:24 +0200

    -

--- nomadicterrain/plants/train.py
+++ nomadicterrain/plants/train.py
@@ -45,10 +45,8 @@ print (head_model.summary())
 with zipfile.ZipFile('/home/burak/Downloads/edible-wild-plants/datasets.zip', 'r') as z:
      im_files = list(z.namelist())
      im_files = [x for x in im_files if ".jpg" in x and "user_images" not in x]
-
-idxs = range(0, len(im_files))
-
-with zipfile.ZipFile('/home/burak/Downloads/edible-wild-plants/datasets.zip', 'r') as z:
+     idxs = range(0, len(im_files))
+     
      def get_batch(batch_size = 30):
           X = np.zeros((batch_size,224,224,3))
           y = np.zeros((batch_size, 62))
@@ -65,7 +63,7 @@ with zipfile.ZipFile('/home/burak/Downloads/edible-wild-plants/datasets.zip', 'r
                y[i,label_idx] = 1.0
           return X, y
      
-     for i in range(100):
+     for i in range(2):
           X, y = get_batch()
           head_model.fit(X, y, epochs=1, batch_size=30, verbose=1)
 
