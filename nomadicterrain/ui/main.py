46be65acb9418a0c6f5ef4b30b930fcfa55de162
Burak Bayramli <burak@localhost.com>
2018-10-31 15:06:11 +0100

    -

--- nomadicterrain/ui/main.py
+++ nomadicterrain/ui/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 import numpy as np
+import pandas as pd
 import sys; sys.path.append("../map")
 import plot_map
 
@@ -13,9 +14,23 @@ def index():
 def location(coordinates):
     lat,lon = coordinates.split(";")
     pts = np.array([[lat, lon]]).astype(float)
-    print (pts)
     plot_map.plot(pts,'static/out.png')    
     return render_template('/location.html', location=coordinates)
+
+@app.route('/parks/<coordinates>')
+def parks(coordinates):
+    lat,lon = coordinates.split(";")
+    pt = np.array([[lat, lon]]).astype(float)
+    df = pd.read_csv(plot_map.dir + "/" + "national_parks.csv", sep='|')
+    
+    for x in df.index:
+        print ('inside parks -------------------')
+        parks = eval(df.ix[x,'Polyline'])
+        l = pt + np.array(parks)
+        print (l)
+                
+#    plot_map.plot(pts,'static/out.png')    
+    return render_template('/parks.html', location=coordinates)
 
 if __name__ == '__main__':
     app.debug = True
