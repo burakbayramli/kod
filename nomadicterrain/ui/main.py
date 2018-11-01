0500c70d5c79436041c22081b5d2803e12f85cdd
Burak Bayramli <burak@localhost.com>
2018-11-01 11:02:39 +0100

    -

--- nomadicterrain/ui/main.py
+++ nomadicterrain/ui/main.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template
+from shapely.geometry import Polygon
 import numpy as np, pandas as pd, os
 import sys; sys.path.append("../map")
-import plot_map, json, random
+import plot_map, json, random, geopy.distance
 
 app = Flask(__name__)
 
@@ -21,25 +22,27 @@ def location(coordinates):
     lat,lon = (float(df.tail(1).lat), float(df.tail(1).lon))
     pts = np.array([[lat, lon]]).astype(float)
     fout = "static/out-%d.png" % int(random.random()*1000000)
-    plot_map.plot(pts, fout ) 
+    plot_map.plot(pts, fout, params['mapzip'] ) 
     return render_template('/location.html', location=fout)
 
 @app.route('/parks/<coordinates>')
 def parks(coordinates):
     df = pd.read_csv(params['gps'])
-    print (df.tail(1).lat)
-    print (df.tail(1).lon)
     lat,lon = (float(df.tail(1).lat), float(df.tail(1).lon))
     pt = np.array([[lat, lon]]).astype(float)
     df = pd.read_csv(params['nationalpark'], sep='|')
-    
+    parks = []
     for x in df.index:
-        print ('inside parks -------------------')
-        parks = eval(df.ix[x,'Polyline'])
-        print (parks)
-                
-#    plot_map.plot(pts,'static/out.png')    
-    return render_template('/parks.html', location=coordinates)
+        ps = eval(df.ix[x,'Polyline'])
+        p = Polygon(ps)
+        dist = geopy.distance.vincenty((p.centroid.x,p.centroid.y),(lat,lon))
+        if dist.km < 100: 
+            parks.append(ps)                
+
+    fout = "static/out-%d.png" % int(random.random()*1000000)
+    plot_map.plot_area(pt, parks, fout, params['mapzip']) 
+    return render_template('/parks.html', location=fout)
+
 
 if __name__ == '__main__':
     app.debug = True
