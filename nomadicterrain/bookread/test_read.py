f07e0a655dc1388cb576277e85b2877463750f9d
Burak Bayramli <burak@localhost.com>
2018-11-02 11:50:20 +0100

    -

--- nomadicterrain/bookread/test_read.py
+++
@@ -1,9 +0,0 @@
-import textract, pyttsx3
-from gtts import gTTS
-import os
-
-text = textract.process("shipof.epub")
-t = str(text[10000:20000] )
-tts = gTTS(text=t, lang='en')
-tts.save("good.mp3")
-
