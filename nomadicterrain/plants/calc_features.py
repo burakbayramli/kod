967ca3a55d1f74867ce26040c035f41bb88f1bc7
Burak Bayramli <burak@localhost.com>
2018-11-06 09:38:47 +0100

    -

---
+++ nomadicterrain/plants/calc_features.py
@@ -0,0 +1,19 @@
+from keras.applications.vgg16 import VGG16, preprocess_input
+from keras.applications.vgg19 import VGG19, preprocess_input
+from keras.applications.xception import Xception, preprocess_input
+from keras.applications.resnet50 import ResNet50, preprocess_input
+from keras.applications.inception_resnet_v2 import InceptionResNetV2, preprocess_input
+from keras.applications.mobilenet import MobileNet, preprocess_input
+from keras.applications.inception_v3 import InceptionV3, preprocess_input
+from keras.preprocessing import image
+from keras.models import Model
+from keras.models import model_from_json
+from keras.layers import Input
+
+base_model = VGG16(weights="imagenet")
+model = Model(input=base_model.input, output=base_model.get_layer('fc1').output)
+image_size = (224, 224)
+
+features = []
+labels   = []
+
