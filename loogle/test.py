e0982881f42c42cfc85562fac5ab01282317dd11
Burak Bayramli <burak@localhost.com>
2018-11-28 11:10:52 +0100

    -

--- loogle/test.py
+++ loogle/test.py
@@ -1,8 +1,25 @@
-import loogle2
+import sys; sys.path.append('..')
+import loogle2, rsync
 
-index_dir = "/tmp/idx"    
-loogle2.index("/home/burak/Documents/kod/loogle/sub", index_dir, new_index=True)
-loogle2.index("/home/burak/Documents/kod/loogle/sub", index_dir)
-res = loogle2.search("scientist", index_dir)
-print (res)
+index_dir = "/tmp/idx"
+cdir = "/home/burak/Documents/kod/loogle/sub"
 
+def test_simple():
+    loogle2.index(cdir, index_dir, new_index=True)
+    loogle2.index(cdir, index_dir)
+    res = loogle2.search("scientist", index_dir)
+    print (res)
+
+def test_add_new():
+    loogle2.index(cdir, index_dir, new_index=True)
+    fout = open(cdir + "/new_book.txt","w")
+    fout.write("doctor something something blabla scientist")
+    fout.close()
+    loogle2.index(cdir, index_dir)
+    res = loogle2.search("doctor", index_dir)
+    print (res)
+    rsync.deleteFile(cdir + "/new_book.txt")
+
+if __name__ == "__main__":
+    #test_simple()
+    test_add_new()
