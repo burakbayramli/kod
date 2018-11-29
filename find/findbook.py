d952987f4ef3c67d9d6130e294fab7ab164067d5
Burak Bayramli <burak@localhost.com>
2018-11-29 16:20:02 +0100

    -

--- find/findbook.py
+++ find/findbook.py
@@ -2,7 +2,7 @@ import sys; sys.path.append('/home/burak/Documents/kod/loogle/')
 import sys; sys.path.append('/home/burak/Documents/kod')
 import loogle3, rsync
 
-index_db = "/home/burak/Downloads/loogle.db"
+index_db = "/home/burak/Documents/Dropbox/loogle.db"
 print (sys.argv[1])
 
 res = loogle3.search(sys.argv[1], index_db)
