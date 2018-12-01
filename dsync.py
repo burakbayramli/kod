78406cdf9ac9a5958daa2aa60122a6469ab5128c
Burak Bayramli <burak@localhost.com>
2018-12-01 19:23:49 +0100

    -

--- dsync.py
+++ dsync.py
@@ -10,6 +10,12 @@ if sys.argv[1] == "hd":
     os.system("python rsync.py '/home/burak/Documents/classnotes' '/media/burak/Seagate Backup Plus Drive/archive/classnotes' --delete 1")
     os.system("python rsync.py '/home/burak/Documents/backlog' '/media/burak/Seagate Backup Plus Drive/archive/backlog' --delete 1")
 
+if sys.argv[1] == "acer_usb64":
+    os.system("python rsync.py '/home/burak/Documents/Dropbox' '/media/burak/1BC3-0618/archive/Dropbox' --delete 1")
+    os.system("python rsync.py '/home/burak/Documents/kod' '/media/burak/1BC3-0618/archive/kod' --delete 1")
+    os.system("python rsync.py '/home/burak/Documents/classnotes' '/media/burak/1BC3-0618/archive/classnotes' --delete 1")
+    os.system("python rsync.py '/home/burak/Documents/backlog' '/media/burak/1BC3-0618/archive/backlog' --delete 1")
+    
 if sys.argv[1] == "hd2usb64":
     os.system("python rsync.py '/media/burak/Seagate Backup Plus Drive/archive' '/media/burak/1BC3-0618/archive' --delete 1")
     os.system("python rsync.py '/media/burak/Seagate Backup Plus Drive/kitaplar' '/media/burak/1BC3-0618/kitaplar' --delete 1")
--- loogle/README.md
+++ loogle/README.md
@@ -6,10 +6,10 @@ Indexes all pdf, djvu, txt, epub files under a given directory, saves
 the index, and allows search on these indexed documents. Allows
 updates, and deletes of files already indexed.
 
-Point to any directory, specify a directory for index database to be
-stored, loogle will index all books, documents for you. Content
-extraction is done through textract which can extract files from
-pretty much all common file formats.
+Point to any directory, specify an index database name, loogle will
+index all books, documents for you. Content extraction is done through
+textract which can extract files from pretty much all common file
+formats.
 
 Loogle can detect additions, removals. TBD: file size changes
 triggering incremental updates.
