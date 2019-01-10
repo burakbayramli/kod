afb797df244abd9e5afe3fcd0b86fe44569bc0ba
BB <me@test.com>
2019-01-10 12:00:06 +0300

    -

--- news.py
+++ news.py
@@ -51,7 +51,7 @@ def getnews(outfile):
                 if len(re.findall(r"Turkish", title, re.IGNORECASE)) > 0: continue
                 if len(re.findall(r"T.rkei", title, re.IGNORECASE)) > 0: continue
                 if len(re.findall(r"Erdo.an", title, re.IGNORECASE)) > 0: continue
-                if len(re.findall(r"Obama", title, re.IGNORECASE)) > 0: continue
+                if len(re.findall(r"Ocasio", title, re.IGNORECASE)) > 0: continue
                 fout.write("<a href='%s'>%s</a><br/><br/>\n" % (link, title))
                 fout.write("%s<br/><br/>\n" % (summary))
             except Exception as e:
