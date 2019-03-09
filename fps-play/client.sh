df9126043d44a10328ce6599b7a77f60a3c82721
Burak Bayramli <burak@localhost.com>
2019-03-09 12:26:11 +0300

    -

--- fps-play/client.sh
+++
@@ -1,4 +0,0 @@
-cd /home/burak/Downloads/UrbanTerror
-# always start at 0,0 position, top left corner.
-export SDL_VIDEO_WINDOW_POS="0,0"
-./ioUrbanTerror.i386 +connect 127.0.0.1
--- istanbulbus/scripts/key.sh
+++
@@ -1,1 +0,0 @@
-/usr/lib/jvm/java-6-sun/bin/keytool -genkey -v -keystore my-release-key.keystore -alias alias_name -keyalg RSA -validity 10000
