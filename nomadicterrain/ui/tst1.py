8cc9a49ab7d7618b30f26919b85f348fc472cda8
Burak Bayramli <burak@localhost.com>
2019-04-16 13:28:30 +0300

    -

--- nomadicterrain/ui/tst1.py
+++ nomadicterrain/ui/tst1.py
@@ -3,16 +3,20 @@ import numpy as np
 from urllib.request import urlopen
 import os, json
 
-#place_query = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=40.250457,28.958647999999997&radius=1500&type=restaurant&key=AIzaSyCxQopw-CIBAdyUrhYk18LC_qurTjUWWlE"
-place_query = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=40.250457,28.958647999999997&radius=30000&keyword=&type=campground&key=AIzaSyCxQopw-CIBAdyUrhYk18LC_qurTjUWWlE"
+place_query = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=%s&radius=30000&keyword=&type=%s&key=%s"
 
 params = json.loads(open(os.environ['HOME'] + "/.nomadicterrain").read())
 
 lat,lon = 40.250457, 28.958647999999997
 location = "%s,%s" % (lat,lon)
-url = place_query
-url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=40.250457,28.958647999999997&radius=30500&type=campsite&keyword=&key=AIzaSyCxQopw-CIBAdyUrhYk18LC_qurTjUWWlE"
-print (url)
-html = urlopen(url)
-json_res = json.loads(html.read().decode('utf-8'))
-print (json_res)
+for type in ['campground','atm','bus_station','shopping_mall']:
+    url = place_query % (location, type, params['api'])
+    print (url)
+    html = urlopen(url)
+    json_res = json.loads(html.read().decode('utf-8'))
+    for x in json_res['results']:
+        olat = x['geometry']['location']['lat']
+        olon = x['geometry']['location']['lng']
+        line = "%s|%s|%s|%s|[%s,%s]" % (type,"X",x['name'],"Single",olat,olon)
+        print (line)
+
