3032f97a01a93ac130c47397a709c9ef4fc3215e
Burak Bayramli <burak@localhost.com>
2019-04-26 10:37:17 +0300

    -

--- nomadicterrain/map/route.py
+++ nomadicterrain/map/route.py
@@ -181,9 +181,7 @@ def get_elev_int(latint, lonint):
     sql = "SELECT lat,lon FROM ELEVATION WHERE latint=%d and lonint=%d and elevation is NULL" % (latint,lonint)
     res = c.execute(sql)
     res = list(res)
-    #N = random.choice([2,40,50,60,70,80])
     N = 40
-    print ('N',N)
     for chunk in chunks(res, N):
         elev_results = get_elev_data_1(chunk)
         for i in range(N):
@@ -334,40 +332,21 @@ def get_all_countries():
                 get_elev_data(lat,lon,rbf=False)
 
 def get_centroid(poly):
-    """Calculates the centroid of a non-intersecting polygon.
-    Args:
-        poly: a list of points, each of which is a list of the form [x, y].
-    Returns:
-        the centroid of the polygon in the form [x, y].
-    Raises:
-        ValueError: if poly has less than 3 points or the points are not
-                    formatted correctly.
-    """
-    # Make sure poly is formatted correctly
     if len(poly) < 3:
         raise ValueError('polygon has less than 3 points')
     for point in poly:
         if type(point) is not list or 2 != len(point):
             raise ValueError('point is not a list of length 2')
-    # Calculate the centroid from the weighted average of the polygon's
-    # constituent triangles
     area_total = 0
     centroid_total = [float(poly[0][0]), float(poly[0][1])]
     for i in range(0, len(poly) - 2):
-        # Get points for triangle ABC
         a, b, c = poly[0], poly[i+1], poly[i+2]
-        # Calculate the signed area of triangle ABC
         area = ((a[0] * (b[1] - c[1])) +
                 (b[0] * (c[1] - a[1])) +
                 (c[0] * (a[1] - b[1]))) / 2.0;
-        # If the area is zero, the triangle's line segments are
-        # colinear so we should skip it
         if 0 == area:
             continue
-        # The centroid of the triangle ABC is the average of its three
-        # vertices
         centroid = [(a[0] + b[0] + c[0]) / 3.0, (a[1] + b[1] + c[1]) / 3.0]
-        # Add triangle ABC's area and centroid to the weighted average
         centroid_total[0] = ((area_total * centroid_total[0]) +
                              (area * centroid[0])) / (area_total + area)
         centroid_total[1] = ((area_total * centroid_total[1]) +
