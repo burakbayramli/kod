00212ae7fb178f279b06847a2a240f66ca75e986
Burak Bayramli <burak@localhost.com>
2019-07-24 08:51:43 +0300

    -

--- nomadicterrain/sql/tst6.py
+++ nomadicterrain/sql/tst6.py
@@ -4,11 +4,13 @@ import pandas as pd
 
 SROWS = 40000
 
-connmain = sqlite3.connect('/media/burak/Seagate Backup Plus Drive/archive/data/campdata/elev.db')
+#connmain = sqlite3.connect('/media/burak/Seagate Backup Plus Drive/archive/data/campdata/elev.db')
+connmain = sqlite3.connect('/home/burak/Downloads/elev.db')
 
 longmin,latmin,longmax,latmax =  2.553,56.15,31.833,71.5
 print (longmin,latmin,longmax,latmax)
-
+count = 0
+total = 0
 for lonint in (range(int(longmin),int(longmax)+1)):
     for latint in (range(int(latmin),int(latmax)+1)):
         print (latint,lonint)
@@ -17,15 +19,17 @@ for lonint in (range(int(longmin),int(longmax)+1)):
         res = connmain.execute(sql)
         res = list(res)
         print(res)
+        if res[0][0] == SROWS: count += 1
         
         sql = "SELECT avg(elevation) FROM ELEVATION WHERE latint=%d and lonint=%d" % (latint,lonint)
         res = connmain.execute(sql)
         res = list(res)
         print(res[0][0])
-        
+        total += 1
         if res[0][0]!=SROWS:        
             pass
         else:
             print ('skipping...')
             continue
-
+        
+print (count, total, count / total)
