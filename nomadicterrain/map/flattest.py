cf4fd910740a8d88d5251d41d0319a23df06e867
BB <burak@localhost.com>
2019-10-22 10:54:28 +0300

    -

--- nomadicterrain/map/flattest.py
+++ nomadicterrain/map/flattest.py
@@ -1,15 +1,68 @@
+from scipy.interpolate import Rbf
 import numpy as np, plot_map, json, os
 import geopy.distance, math, route
 from datetime import timedelta
 import datetime, sqlite3
 
+SROWS = 40000
 params = json.loads(open(os.environ['HOME'] + "/Downloads/campdata/nomterr.conf").read())
 
-conn = sqlite3.connect(params['elevdb'])
-connmod = sqlite3.connect(params['elevdbmod'])
+def insert_rbf_recs(latint,lonint,conn,connmod):
+    c = conn.cursor()    
+    cm = connmod.cursor()    
+    sql = "DELETE FROM RBF1 where latint=%d and lonint=%d" % (latint, lonint)
+    cm.execute(sql)
+    connmod.commit()
+    sql = "SELECT lat,lon,elevation FROM ELEVATION WHERE latint=%d and lonint=%d " % (latint,lonint)
+    res = c.execute(sql)
+    X = []; Z=[]
+    for (lat,lon,elevation) in res:
+        if ".1" in str(lat) and ".2" in str(lon): 
+            #print (lat,lon,elevation)
+            X.append([lon,lat])
+            Z.append([elevation])
+    
+    X = np.array(X)
+    Z = np.array(Z)
+    X = X[Z[:,0]>0.0]
+    Z = Z[Z[:,0]>0.0]
+    print (X.shape)
+    rbfi = Rbf(X[:,0], X[:,1], Z)
 
-lat1,lon1 = 41.084967,31.126588
-lat1 = float(lat1)
-lon1 = float(lon1)
-lat2,lon2 = 40.749752,31.610694
+def do_all_rbf_ints():
 
+    conn = sqlite3.connect(params['elevdb'])
+    connmod = sqlite3.connect(params['elevdbmod'])
+
+    lat1,lon1 = 41.084967,31.126588
+    lat1 = float(lat1)
+    lon1 = float(lon1)
+    lat2,lon2 = 40.749752,31.610694
+    
+    c = conn.cursor()
+    #c.execute("delete from RBF1")
+    res = c.execute('''select distinct latint, lonint from elevation; ''')
+
+    for (latint,lonint) in res:
+        print ('int---->', latint,lonint)
+
+        sql1 = "SELECT count(*) FROM ELEVATION WHERE latint=%d and lonint=%d; " % (latint,lonint)
+        c2 = conn.cursor()
+        res1 = c2.execute(sql1)
+        res1 = list(res1)
+
+        sql2 = "select count(*) from RBF1 where latint=%d and lonint=%d; "  % (latint,lonint)
+        c3 = connmod.cursor()
+        res2 = c3.execute(sql2)
+        res2 = list(res2)
+
+        print(res1[0][0], res2[0][0])       
+        insert_rbf_recs(latint,lonint,conn,connmod)        
+        break
+
+    c.close()
+    conn.close()
+    connmod.close()
+
+
+do_all_rbf_ints()    
