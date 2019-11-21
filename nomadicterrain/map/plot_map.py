0d5bee2d4dbd3267d6f411e599ebd1cfac72393e
BB <burak@localhost.com>
2019-11-21 19:43:21 +0300

    -

--- nomadicterrain/map/plot_map.py
+++ nomadicterrain/map/plot_map.py
@@ -152,9 +152,9 @@ def plot_topo(lat1,lon1,fout1,fout2,fout3,how_far):
 
     plon,plat = np.round(float(lon1),3),np.round(float(lat1),3)
 
-    from scipy.ndimage.filters import gaussian_filter
-    sigma = 0.7
-    zz = gaussian_filter(zz, sigma)
+    #from scipy.ndimage.filters import gaussian_filter
+    #sigma = 0.7
+    #zz = gaussian_filter(zz, sigma)
     
     plt.figure()
     plt.plot(plon,plat,'rd')
