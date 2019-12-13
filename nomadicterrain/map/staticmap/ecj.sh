fd7c57f45b06fe05dcaa4aa10c32715150bf677f
BB <burak@localhost.com>
2019-12-13 20:56:23 +0300

    -

--- nomadicterrain/map/staticmap/ecj.sh
+++
@@ -1,6 +0,0 @@
-DIR=$HOME/Documents/kod/nomadicterrain/map/staticmap
-ecj -cp $DIR/build:$DIR/lib/kxml2-2.3.0.jar:$DIR/lib/mapsforge-core-0.12.0.jar:\
-$DIR/lib/mapsforge-map-0.12.0.jar:$DIR/lib/mapsforge-map-awt-0.12.0.jar:\
-$DIR/lib/mapsforge-map-reader-0.12.0.jar:$DIR/lib/mapsforge-themes-0.12.0.jar:\
-$DIR/lib/svg-salamander-1.0.jar \
-src/SaveTiles.java 
--- nomadicterrain/map/staticmap/run2.sh
+++
@@ -1,6 +0,0 @@
-DIR=$HOME/Documents/kod/nomadicterrain/map/staticmap
-dalvikvm -cp $DIR/build/build.dex:$DIR/lib/kxml2-2.3.0.dex:$DIR/lib/mapsforge-core-0.12.0.dex:\
-$DIR/lib/mapsforge-map-0.12.0.dex:$DIR/lib/mapsforge-map-awt-0.12.0.dex:\
-$DIR/lib/mapsforge-map-reader-0.12.0.dex:$DIR/lib/mapsforge-themes-0.12.0.dex:\
-$DIR/lib/svg-salamander-1.0.dex \
-SaveTiles $1 $2 $3 $4
