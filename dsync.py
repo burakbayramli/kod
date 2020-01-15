8d296d54c5842e7a7980a40257faaa6fbdcdbda0
BB <burak@localhost.com>
2020-01-15 15:39:57 +0300

    -

--- dsync.py
+++ dsync.py
@@ -4,7 +4,7 @@ if len(sys.argv) < 2:
     print ("Usage dsync.py [letter]")
     exit()
 
-if sys.argv[1] == "acer_hd":
+if sys.argv[1] == "acer_ext":
     os.system("python rsync.py '/home/burak/Documents/Dropbox' '/media/burak/Seagate Backup Plus Drive/archive/Dropbox' --delete 1")
     os.system("python rsync.py '/home/burak/Documents/thirdwave' '/media/burak/Seagate Backup Plus Drive/archive/repos/thirdwave'  --ignore-list=.git --delete 1")
     os.system("python rsync.py '/home/burak/Documents/kod' '/media/burak/Seagate Backup Plus Drive/archive/kod' --ignore-list=.git --delete 1")
@@ -16,8 +16,9 @@ if sys.argv[1] == "acer_usb64":
     os.system("python rsync.py '/home/burak/Documents/thirdwave' '/media/burak/1BC3-0618/archive/repos/thirdwave'  --ignore-list=.git  --delete 1")
     os.system("python rsync.py '/home/burak/Documents/kod' '/media/burak/1BC3-0618/archive/kod' --ignore-list=.git  --delete 1")
     os.system("python rsync.py '/home/burak/Documents/classnotes' '/media/burak/1BC3-0618/archive/classnotes'  --ignore-list=.git  --delete 1")
+    os.system("python rsync.py '/home/burak/Documents/books' '/media/burak/1BC3-0618/archive/books'  --ignore-list=.git  --delete 1")
     
-if sys.argv[1] == "usb64_hd":
+if sys.argv[1] == "usb64_ext":
     os.system("python rsync.py '/media/burak/1BC3-0618/archive/kitaplar' '/media/burak/Seagate Backup Plus Drive/archive/kitaplar'  --delete 1")
     
 if sys.argv[1] == "usb64_flashblue":
@@ -28,6 +29,6 @@ if sys.argv[1] == "acer_flash":
     #os.system("python rsync.py '/home/burak/Documents/classnotes' '/media/burak/7502-42E6/classnotes' --delete 1")
     os.system("python rsync.py '/home/burak/Documents/Dropbox' '/media/burak/7502-42E6/Dropbox' --delete 1")
     
-if sys.argv[1] == "hd_flashblue":
+if sys.argv[1] == "ext_flashblue":
     os.system("python rsync.py '/media/burak/Seagate Backup Plus Drive/archive/kitaplar' '/media/burak/23B9-71E6/kitaplar' --delete 1")
     
