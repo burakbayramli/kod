4b7517cdef93e267baed241f5fd4aaec3ac8b6ab
BB <burak@localhost.com>
2020-04-26 22:18:16 +0300

    -

--- blog.py
+++ blog.py
@@ -1,4 +1,9 @@
 import markdown, sys, os
-#content=open("/data/data/com.termux/files/home/Documents/Dropbox/blogs/sk/2016/01/python-ile-finans-verileri.md").read()
+head = '''
+<script type="text/x-mathjax-config">MathJax.Hub.Config({  tex2jax: {inlineMath: [["$","$"],["\\(","\\)"]]}});</script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/MathJax.js?config=TeX-AMS_HTML-full">
+</script>
+'''
 content=open(sys.argv[1]).read()
+print (head)
 print (markdown.markdown(content, extensions=['fenced_code']))
--- blog.sh
+++ blog.sh
@@ -1,2 +1,1 @@
 python $HOME/Documents/kod/blog.py $1 > $HOME/Downloads/out.html
-#perl -pi -e 's/<\/p>/<\/p><br\/>/g' $HOME/Downloads/out.html
