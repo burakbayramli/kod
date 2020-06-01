7771759b75bddd5665a10f09d031e57ceb0dea42
BB <burak@localhost.com>
2020-06-01 20:38:18 +0300

    -

--- blog.sh
+++ blog.sh
@@ -1,5 +1,4 @@
 python $HOME/Documents/kod/blog.py $1 > `pwd`/out.html
 xvkbd -window *Firefox* -text "\Cr"
-xvkbd -window *emacs* -text "\Cg"
-
-
+xvkbd -window *emacs* -text "\Ce"
+sort $1 | uniq -cd | sort -nr
