9a1f50bd4a099cec9e846cf1d3800e293024c965
BB <you@example.com>
2020-07-15 11:48:38 +0300

    -

--- blog.sh
+++ blog.sh
@@ -1,4 +1,4 @@
 python $HOME/Documents/kod/blog.py $1 > `pwd`/out.html
-xvkbd -window *Firefox* -text "\Cr" > /dev/null 2>&1
-xvkbd -window *emacs* -text "\Ce" > /dev/null 2>&1
+#xvkbd -window *Firefox* -text "\Cr" > /dev/null 2>&1
+#xvkbd -window *emacs* -text "\Ce" > /dev/null 2>&1
 sort $1 | uniq -cd | sort -nr
