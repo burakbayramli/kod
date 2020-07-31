d39693f1a844090b3a8a82862eea70cee9566ad4
BB <you@example.com>
2020-07-31 19:40:28 +0300

    -

--- giftext.py
+++ giftext.py
@@ -1,13 +1,13 @@
 import subprocess, os
 
 w = '"fps=10,scale=360:-1:flags=lanczos,'
-w += "drawtext=enable='between(t,0,2)':fontfile=font3.ttf:text='Txt1':fontsize=15:fontcolor=white:x=(w-tw)/2:y=(h/PHI)+th,"
-w += "drawtext=enable='between(t,2,5)':fontfile=font3.ttf:text='Txt2':fontsize=15:fontcolor=white:x=(w-tw)/2:y=(h/PHI)+th,"
-w += "drawtext=enable='between(t,5,7)':fontfile=font3.ttf:text='Txt3':fontsize=15:fontcolor=white:x=(w-tw)/2:y=(h/PHI)+th,"
-w += "drawtext=enable='between(t,7,9)':fontfile=font3.ttf:text='Txt4':fontsize=15:fontcolor=white:x=(w-tw)/2:y=(h/PHI)+th,"
+w += "drawtext=enable='between(t,0,1)':fontfile=font3.ttf:text='T1':fontsize=15:fontcolor=white:x=30:y=170,"
+w += "drawtext=enable='between(t,1,3)':fontfile=font3.ttf:text='T2':fontsize=15:fontcolor=white:x=30:y=170,"
+w += "drawtext=enable='between(t,3,6)':fontfile=font3.ttf:text='T3':fontsize=15:fontcolor=white:x=30:y=170,"
+w += "drawtext=enable='between(t,6,11)':fontfile=font3.ttf:text='T4':fontsize=15:fontcolor=white:x=30:y=170,"
 w += 'split[x1][x2];[x1]palettegen[p];[x2][p]paletteuse"'
 
-cmd = "/usr/bin/ffmpeg -ss 47 -t 9 -i video.mp4 -filter_complex " + w + " output.gif"
+cmd = "/usr/bin/ffmpeg -ss 48 -t 11 -i vid.mp4 -filter_complex " + w + " output.gif"
 
 print (cmd)
 
