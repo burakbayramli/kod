55d984c14f50c71cb16bd2f35b21708eef72702c
BB <you@example.com>
2020-08-04 09:31:13 +0300

    -

--- dsync.py
+++ dsync.py
@@ -1,4 +1,4 @@
-import os, sys, shutil, rsync, re
+import os, sys, rsync
 
 if len(sys.argv) < 2:
     print ("Usage dsync.py [letter]")
@@ -10,6 +10,8 @@ if sys.argv[1] == "acer_ext":
     os.system("python rsync.py '/home/burak/Documents/kod' '/media/burak/Seagate Backup Plus Drive/archive/kod' --ignore-list=.git --delete 1")
     os.system("python rsync.py '/home/burak/Documents/classnotes' '/media/burak/Seagate Backup Plus Drive/archive/classnotes'  --ignore-list=.git  --delete 1")
     os.system("python rsync.py '/home/burak/Documents/books' '/media/burak/Seagate Backup Plus Drive/archive/repos/books'  --ignore-list=.git")
+    os.system("python rsync.py '/home/burak/Documents/kitaplar' '/media/burak/Seagate Backup Plus Drive/archive/kitaplar'  --delete 1")
+    os.system("python rsync.py '/home/burak/Downloads/campdata/book' '/media/burak/Seagate Backup Plus Drive/archive/data/campdata/book'")
     
 if sys.argv[1] == "acer_usb64":
     os.system("python rsync.py '/home/burak/Documents/Dropbox' '/media/burak/1BC3-0618/archive/Dropbox' --delete 1")
