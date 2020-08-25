1111dee4baa042080f72facd4e97f43bfee4e611
BB <you@example.com>
2020-08-25 10:46:58 +0300

    -

--- find/findbook.py
+++ find/findbook.py
@@ -4,4 +4,4 @@ p = subprocess.Popen(['recoll','-t',sys.argv[1]], stdout=subprocess.PIPE)
 for line in p.stdout:
     res = re.findall('\[file:\/\/(.*?)\]', str(line), re.DOTALL)
     if (len(res)>0):
-        print (":3:%s:308::" % res[0])
+        print ("%s:1:1" % res[0])
