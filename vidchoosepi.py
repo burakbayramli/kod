db34653c4d969fa7e14ebce3b80e784828789eb3
BB <you@example.com>
2020-09-06 16:37:02 +0300

    -

---
+++ vidchoosepi.py
@@ -0,0 +1,13 @@
+import glob, os, random, sys
+import threading, numpy as np
+import datetime, random
+from rsync import ls
+import select, rndplay
+
+print ("Music Dir", sys.argv[1]    )
+dir = "/media/pi/Seagate Backup Plus Drive/shows"
+dirs,list = ls(dir)
+print ("Files", len(list))
+idx = rndplay.my_random(len(list))
+print ("show idx selected", idx, "song", list[idx][0])
+
