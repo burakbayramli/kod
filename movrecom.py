486f093addf3327d235cf830ca9f307c22a6f9f8
MK <me@yomama.com>
2021-03-31 22:07:33 +0300

    -

--- movrecom.py
+++ movrecom.py
@@ -9,24 +9,26 @@
 
 from sklearn.metrics.pairwise import cosine_similarity
 from scipy.sparse import csr_matrix
+import scipy.sparse.linalg
 import pandas as pd, numpy as np
 import os, sys
 
 d = "/media/burak/3d1ece2f-6539-411b-bac2-589d57201626/home/burak/Downloads/ml-latest"
 
+picks = {"Swordfish (2001)": 5.0, "Every Which Way But Loose (1978)": 5.0,
+         "Sideways (2004)": 5.0, "Expendables, The (2010)": 5.0,
+         "Shawshank Redemption, The (1994)": 5.0
+}
+
 if len(sys.argv) < 2:
-    print ("Usage movrecom.py [action]")
+    print ("Usage movrecom.py [normrec|svdrec]")
     exit()
 
-if sys.argv[1] == "rec":
+if sys.argv[1] == "normrec":
     ratings = pd.read_csv(d + "/ratings.csv")
     utility_csr = csr_matrix((ratings.rating, (ratings.userId , ratings.movieId)))
 
     mov = pd.read_csv(d + "/movies.csv",index_col="title")['movieId'].to_dict()
-    picks = {"Swordfish (2001)": 5.0, "Every Which Way But Loose (1978)": 5.0,
-             "Sideways (2004)": 5.0, "Expendables, The (2010)": 5.0,
-             "Shawshank Redemption, The (1994)": 5.0
-    }
     tst = np.zeros((1,utility_csr.shape[1]))
     for p in picks: tst[0,mov[p]] = picks[p]
 
@@ -44,4 +46,25 @@ if sys.argv[1] == "rec":
             if n not in picks and r >= 4.0: res[n] = r 
     for x in res: print (x)
 
+if sys.argv[1] == "svdrec":
+    ratings = pd.read_csv(d + "/ratings.csv")
+    utility_csr = csr_matrix((ratings.rating, (ratings.userId , ratings.movieId)))
 
+    mov = pd.read_csv(d + "/movies.csv",index_col="title")['movieId'].to_dict()
+
+    for p in picks: utility_csr[0,mov[p]] = picks[p]
+    
+    A = scipy.sparse.linalg.svds(utility_csr, k=10)[0]
+
+    similarities = cosine_similarity(A, A[0,:].reshape(1,10))
+    m = np.argsort(similarities[:,0])
+    movi = pd.read_csv(d + "/movies.csv",index_col="movieId")['title'].to_dict()
+
+    res = {}
+    for idx in range(1,1000):
+        ii,jj = utility_csr[m[-idx],:].nonzero()    
+        for j in jj:
+            r = utility_csr[m[-idx],:][0,j]
+            n = movi[j]
+            if n not in picks and r >= 4.0: res[n] = r 
+    for x in res: print (x)    
