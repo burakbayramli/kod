ca31bd0b7f0f38ca9e4eecde820400dde102deff
BB <me@yomama.com>
2021-11-10 22:04:44 +0300

    -

--- movrecom.py
+++ movrecom.py
@@ -16,7 +16,7 @@ import os, sys, re
 d = "/mnt/3d1ece2f-6539-411b-bac2-589d57201626/home/burak/Downloads/ml-latest"
 
 picks = {
-    "Jupiter Ascending (2015)": 4.0
+    "Vertical Limit (2000)": 5.0
 }
 
 if len(sys.argv) < 2:
@@ -36,14 +36,22 @@ if sys.argv[1] == "normal":
     m = np.argsort(similarities[:,0])
     movi = pd.read_csv(d + "/movies.csv",index_col="movieId")['title'].to_dict()
 
-    res = {}
+    res = []
     for idx in range(1,1000):
         ii,jj = utility_csr[m[-idx],:].nonzero()    
         for j in jj:
             r = utility_csr[m[-idx],:][0,j]
             n = movi[j]
-            if n not in picks and r >= 4.0: res[n] = r 
-    for x in res: print (x)
+            fres = re.findall('\((\d\d\d\d)\)', n)
+            if len(fres)>0 and n not in picks and r >= 4.0:
+                year = int(fres[0])
+                res.append([n, year])
+    df = pd.DataFrame(res)
+    df = df.sort_values(1,ascending=False)
+    fout = '~/Downloads/moviepicks.csv'
+    df = df.drop_duplicates(0)
+    df.to_csv(fout)
+    print ('See ' + fout)
 
 if sys.argv[1] == "svd":
     ratings = pd.read_csv(d + "/ratings.csv")
@@ -72,6 +80,7 @@ if sys.argv[1] == "svd":
     df = pd.DataFrame(res)
     df = df.sort_values(1,ascending=False)
     fout = '~/Downloads/moviepicks.csv'
+    df = df.drop_duplicates(0)
     df.to_csv(fout)
     print ('See ' + fout)
 
