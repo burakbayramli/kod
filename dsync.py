1bf7796f21b6fb8c66d6f81247044a55e8ab2ec9
BB <me@yomama.com>
2022-08-07 21:23:42 +0300

    -

--- dsync.py
+++ dsync.py
@@ -9,20 +9,20 @@ if sys.argv[1] == "acer_ext":
     if (len(tmp)==0):
         print ('\n============= PARTITION NOT MOUNTED =================')
         exit()
-    os.system("python rsync.py '/home/burak/Documents/Dropbox' '/media/burak/Seagate Backup Plus Drive/archive/Dropbox' --delete 1")
-    os.system("python rsync.py '/home/burak/Documents/thirdwave' '/media/burak/Seagate Backup Plus Drive/archive/repos/thirdwave'  --ignore-list=.git --delete 1")
-    os.system("python rsync.py '/home/burak/Documents/kod' '/media/burak/Seagate Backup Plus Drive/archive/kod' --ignore-list=.git --delete 1")
-    os.system("python rsync.py '/home/burak/Documents/classnotes' '/media/burak/Seagate Backup Plus Drive/archive/classnotes'  --ignore-list=.git  --delete 1")
+    os.system("python rsync.py '/home/burak/Documents/Dropbox' '/media/burak/Backup Plus/Dropbox' --delete 1")
+    os.system("python rsync.py '/home/burak/Documents/thirdwave' '/media/burak/Backup Plus/archive/repos/thirdwave'  --ignore-list=.git --delete 1")
+    os.system("python rsync.py '/home/burak/Documents/kod' '/media/burak/Backup Plus/archive/kod' --ignore-list=.git --delete 1")
+    os.system("python rsync.py '/home/burak/Documents/classnotes' '/media/burak/Backup Plus/archive/classnotes'  --ignore-list=.git  --delete 1")
     #os.system("python rsync.py '/home/burak/Documents/books' '/media/burak/Seagate Backup Plus Drive/archive/repos/books'  --ignore-list=.git")
-    os.system("python rsync.py '/home/burak/Documents/kitaplar' '/media/burak/Seagate Backup Plus Drive/archive/kitaplar'  --delete 1")
+    os.system("python rsync.py '/home/burak/Documents/kitaplar' '/media/burak/Backup Plus/archive/kitaplar'  --delete 1")
     #os.system("python rsync.py '/home/burak/Downloads/campdata' '/media/burak/Seagate Backup Plus Drive/archive/data/campdata'  --delete 1")
     
-if sys.argv[1] == "ext1_ext2":
-    os.system("python rsync.py '/media/burak/Seagate Backup Plus Drive/shows' '/media/burak/Backup Plus/shows'  --delete 1")
-    os.system("python rsync.py '/media/burak/Seagate Backup Plus Drive/archive/Dropbox' '/media/burak/Backup Plus/archive/Dropbox'  --delete 1")
-    os.system("python rsync.py '/media/burak/Seagate Backup Plus Drive/archive/data' '/media/burak/Backup Plus/archive/data'  --delete 1")
-    os.system("python rsync.py '/media/burak/Seagate Backup Plus Drive/archive/kitaplar' '/media/burak/Backup Plus/archive/kitaplar' --delete 1")
-    os.system("python rsync.py '/media/burak/Seagate Backup Plus Drive/Lectures' '/media/burak/Backup Plus/Lectures' ")
+#if sys.argv[1] == "ext1_ext2":
+#    os.system("python rsync.py '/media/burak/Seagate Backup Plus Drive/shows' '/media/burak/Backup Plus/shows'  --delete 1")
+#    os.system("python rsync.py '/media/burak/Seagate Backup Plus Drive/archive/Dropbox' '/media/burak/Backup Plus/archive/Dropbox'  --delete 1")
+#    os.system("python rsync.py '/media/burak/Seagate Backup Plus Drive/archive/data' '/media/burak/Backup Plus/archive/data'  --delete 1")
+#    os.system("python rsync.py '/media/burak/Seagate Backup Plus Drive/archive/kitaplar' '/media/burak/Backup Plus/archive/kitaplar' --delete 1")
+#    os.system("python rsync.py '/media/burak/Seagate Backup Plus Drive/Lectures' '/media/burak/Backup Plus/Lectures' ")
     
 if sys.argv[1] == "acer_usb64":
     tmp = glob.glob('/home/burak/Documents/kitaplar/*')
