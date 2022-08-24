915ee4f1f95a3ae0560672acd02036efeb1d4ec6
BB <me@yomama.com>
2022-08-24 14:52:23 +0300

    -

--- nomadicterrain/util.py
+++ nomadicterrain/util.py
@@ -1,4 +1,4 @@
-import re
+import re, folium, pandas as pd
 from bs4 import BeautifulSoup as bs
 from unidecode import unidecode
 
@@ -9,7 +9,7 @@ def kamp_yerleri():
     content = open("/tmp/trkamp.kml").read()
     fout = open("/tmp/kampyerleri.csv","w")
     res = re.findall(r'<name>(.*?)</name>.*?<description>(.*?)</description>.*?<coordinates>(.*?)</coordinates>', content, re.DOTALL)
-    fout.write("%s|%s|%s\n" % ("name","description","location"))
+    fout.write("%s;%s;%s\n" % ("name","location","description"))
     for i,x in enumerate(res):
         if i==0: continue
         soup=bs(x[1],'lxml')
@@ -18,13 +18,27 @@ def kamp_yerleri():
         txt = txt.replace("]]>","")
         txt = txt.replace("\n","").strip()
         txt = txt.replace("|", " ")
+        txt = txt.replace(";", " ")
+        txt = unidecode(txt)
         name = x[0].strip()
         name = name.replace("<![CDATA[","")
         name = name.replace("]]>","")
-        fout.write("%s;%s;%s\n" % (name,x[2].strip().replace(",0",""),unidecode(txt)))
+        loc = x[2].strip().replace(",0","")
+        fout.write("%s;%s;%s\n" % (name,loc,txt))
         fout.flush()
 
     fout.close()
 
+def camp_folium():
+    df = pd.read_csv('data/kampyerleri.csv',sep=';')
+    m = folium.Map(location=[39, 33], zoom_start=7, tiles="Stamen Terrain")
+    for index, row in df.iterrows():
+        lat,lon = (float(x) for x in row['location'].split(","))
+        folium.Marker(
+            [lon,lat], popup=row['description'],
+        ).add_to(m)
+    m.save('static/trcamp-out.html')
+    
 if __name__ == "__main__": 
-    kamp_yerleri()
+    #kamp_yerleri()
+    camp_folium()
