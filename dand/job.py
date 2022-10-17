1e895343b406fe057f7ced80afcd9f4e99660677
BB <me@yomama.com>
2022-10-17 14:05:14 +0300

    -

---
+++ dand/job.py
@@ -0,0 +1,27 @@
+import os
+
+def process(file_name,ci,N,hookobj,skip_lines=0):
+    file_size = os.path.getsize(file_name)
+    with open(file_name, 'r') as f:
+        for j in range(skip_lines): f.readline()
+        beg = f.tell()
+        f.close()
+    chunks = []
+    for i in range(N):
+        with open(file_name, 'r') as f:
+            s = int((file_size / N)*(i+1))
+            f.seek(s)
+            f.readline()
+            end_chunk = f.tell()-1
+            chunks.append([beg,end_chunk])
+            f.close()
+        beg = end_chunk+1
+    c = chunks[ci]
+    with open(file_name, 'r') as f:
+        f.seek(c[0])
+        while True:
+            line = f.readline()
+            hookobj.exec(line)
+            if f.tell() > c[1]: break
+        f.close()
+        hookobj.post()
