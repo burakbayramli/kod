8c4483ab92f859715605dfe3a6765e9222f6008c
BB <me@yomama.com>
2023-02-23 10:00:53 +0300

    -

--- nomadicterrain/news.py
+++ nomadicterrain/news.py
@@ -9,7 +9,7 @@ def strip_html(input):
 skip_words = ["Turk", "Türkiye", "battery","Webb", "electric","Blinken","Biden",
               "lithium", "AOC", "der Leyen", "Erdo.an","Elon", "Musk","Tesla",
               "batteries", "SpaceX", "Mars","black hole", "artificial intelligence",
-              " AI ", "AI ", "poll", "Zelensky", "black hole", " EV ", "Webb",
+              " AI ", "AI ", " AI", "poll", "Zelensky", "black hole", " EV ", "Webb",
               "A\.I\.", "telescope", "Yellen", "Francis","Thunberg","tweet",
               "tweets","twitter","ChatGPT"]
 
@@ -23,11 +23,12 @@ def getnews():
         ("Independent, The", "http://www.independent.co.uk/news/world/rss", 10),
         ("The Guardian","http://www.theguardian.com/world/rss",10),
         ('Al Monitor','https://www.al-monitor.com/rss',20),
+        ('Euractiv','https://www.euractiv.com/?feed=mcfeed',10),
         ("TASS","http://tass.com/rss/v2.xml",20),
         ("Arab News","https://www.arabnews.com/cat/3/rss.xml",10),
         ("Jane's Defence", "https://www.janes.com/feeds/news", 10),        
         ('CNBC','https://www.cnbc.com/id/100727362/device/rss/rss.html',10),        
-        ('WSJ','https://feeds.a.dj.com/rss/RSSWorldNews.xml',10),
+        ('WSJ','https://feeds.a.dj.com/rss/RSSOpinion.xml',10),
         ("NYT", "https://rss.nytimes.com/services/xml/rss/nyt/World.xml",15),
         ("WION","https://www.wionews.com/feeds/world/rss.xml",20),        
         ("TFI Global","https://tfiglobalnews.com/feed/",20),
