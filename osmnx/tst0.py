e50c8c73b5328389264ed7183c4cb6b49e2811f4
BB <me@yomama.com>
2023-05-10 08:51:55 +0300

    -

--- osmnx/tst0.py
+++
@@ -1,6 +0,0 @@
-import osmnx as ox, networkx as nx
-
-G = ox.graph_from_xml("/opt/Downloads/osm/seychelles-latest.osm.bz2")
-nx.write_edgelist(G, "test.edgelist.gz",data=["oneway","length"])
-
-print (len(G))
