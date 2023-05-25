5e4f34873b55f8a5944be15f0b2dabb7d697213d
BB <me@yomama.com>
2023-05-25 09:10:22 +0300

    -

--- nomadicterrain/loogle.py
+++ nomadicterrain/loogle.py
@@ -1,3 +1,33 @@
+"""
+Loogle - Your Local Google
+
+Index and search your local hard drive, a pure python replacement for
+recoll.
+
+Indexes all pdf, djvu, txt, epub files under a given directory, saves
+the index, and allows search on these indexed documents. 
+
+Point to any directory, specify an index database name, loogle will
+index all books, documents for you. Content extraction is done through
+`textract` which can extract files from pretty much all common file
+formats. Loogle can detect additions, removals.
+
+TODO: file size changes triggering incremental updates.
+
+Requirements
+
+Python packages
+
+`textract`
+`sqlite3`
+
+Programs
+
+`pdftotext`
+`djvutxt`
+
+Install with `apt-get install djulibre-bin  poppler-utils`
+"""
 import os, io, codecs, sqlite3, textract, json, sys
 
 exts = ['.pdf']
@@ -147,5 +177,5 @@ def test_search():
     
 if __name__ == "__main__": 
  
-    #index_nomadic()
-    test_search()
+    index_nomadic()
+    #test_search()
