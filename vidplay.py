45605d36a21f1b073006191eb03f948be5a2ff93
BB <me@yomama.com>
2023-06-03 16:56:53 +0300

    -

--- vidplay.py
+++ vidplay.py
@@ -4,48 +4,44 @@ import datetime, random
 from rsync import ls
 import select, util
 
-dir = ''
-
-if len(sys.argv) > 1 and sys.argv[1] == "pi":
-    dir = "/media/pi/Seagate Backup Plus Drive1/shows"
-    print ('pi')
-else:
-    dir = "/media/burak/Backup Plus/shows"
-    print ('acer')
+def pick_file():
+    dir = "/media/burak/Backup Plus/shows"    
+    dirs,flist = ls(dir)
+    def fin(s,l): return np.any([x in l for x in s])
+    playlist = [f[0] for f in flist if fin(['.mp4','.mkv','.avi'], f[0])]
+    print ('count', len(playlist))
+    playlist = [f for f in playlist if 'SG-1' not in f]
+    playlist = [f for f in playlist if 'DS9' not in f]
+    playlist = [f for f in playlist if 'VOYAGER' not in f]
+    playlist = [f for f in playlist if 'Atlantis' not in f]
+    playlist = [f for f in playlist if 'The Next Generation' not in f]
+    playlist = [f for f in playlist if 'Enterprise' not in f]
+    playlist = [f for f in playlist if 'Firefly' not in f]
+    playlist = [f for f in playlist if 'Expanse' not in f]
+    playlist = [f for f in playlist if 'Lost' not in f]
+    playlist = [f for f in playlist if '/TLS/' not in f]
+    playlist = [f for f in playlist if 'BSG' not in f]
+    playlist = [f for f in playlist if 'Doctor' not in f]
+    print ('filterd count', len(playlist))
+    idx = util.my_random(len(playlist))
+    #idx = util.my_random2(len(playlist))
+    f = playlist[idx]
+    print ("show idx selected", idx)
+    return f
     
-dirs,flist = ls(dir)
+def rnd_play():
+    f = pick_file()
+    cmd = "vlc '%s' -f " % f
+    print (cmd)
+    os.system(cmd)
+        
+def rnd_copy():
+    f = pick_file()
+    cmd = "cp '%s' /home/burak/Downloads/ " % f
+    print (cmd)
+    os.system(cmd)
+    
+if __name__ == "__main__": 
 
-def fin(s,l): return np.any([x in l for x in s])
-
-playlist = [f[0] for f in flist if fin(['.mp4','.mkv','.avi'], f[0])]
-
-print ('count', len(playlist))
-
-playlist = [f for f in playlist if 'SG-1' not in f]
-playlist = [f for f in playlist if 'DS9' not in f]
-playlist = [f for f in playlist if 'VOYAGER' not in f]
-playlist = [f for f in playlist if 'Atlantis' not in f]
-playlist = [f for f in playlist if 'The Next Generation' not in f]
-playlist = [f for f in playlist if 'Enterprise' not in f]
-playlist = [f for f in playlist if 'Firefly' not in f]
-playlist = [f for f in playlist if 'Expanse' not in f]
-playlist = [f for f in playlist if 'Lost' not in f]
-playlist = [f for f in playlist if '/TLS/' not in f]
-playlist = [f for f in playlist if 'BSG' not in f]
-playlist = [f for f in playlist if 'Doctor' not in f]
-
-print ('filterd count', len(playlist))
-
-idx = util.my_random(len(playlist))
-#idx = util.my_random2(len(playlist))
-
-f = playlist[idx]
-
-print ("show idx selected", idx)
-
-cmd = "vlc '%s' -f " % f
-
-print (cmd)
-
-os.system(cmd)
-
+    #rnd_play()
+    rnd_copy()
