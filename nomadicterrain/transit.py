49f87ad0b5dbd659882bd0ea4a86b03c3a1e73f2
BB <me@yomama.com>
2023-07-11 15:56:52 +0300

    -

--- nomadicterrain/transit.py
+++ nomadicterrain/transit.py
@@ -1,24 +1,26 @@
 import urllib.parse, requests
 import csv, numpy as np, re, os
 import pandas as pd, json, folium
+from unidecode import unidecode
 
 params = json.loads(open(os.environ['HOME'] + "/.nomterr.conf").read())
-bus = params['osm_dir'] + "/istbus.json"
-metro = params['osm_dir'] + "/istmetro.json"
+city =  params['osm_city']
+bus = params['osm_dir'] + "/" + unidecode(city.lower()) + "bus.json"
+metro = params['osm_dir'] + "/" + unidecode(city.lower()) + "metro.json"
 base_url = "https://overpass-api.de/api/interpreter?data="
 
 def get_bus():
 
     q = """
     [out:json];
-    area[name="İstanbul"]->.a;
+    area[name="%s"]->.a;
     (
       nwr["route"="bus"](area.a);
     );
     out body;
     >;
     out skel;
-    """
+    """ % city
 
     safe_string = urllib.parse.quote_plus(q)
     r = requests.get(base_url + safe_string)
@@ -30,14 +32,14 @@ def get_metro():
 
     q = """
     [out:json];
-    area[name="İstanbul"]->.a;
+    area[name="%s"]->.a;
     (
       nwr["route"="subway"](area.a);
     );
     out body;
     >;
     out skel;
-    """
+    """ % city
 
     safe_string = urllib.parse.quote_plus(q)
     r = requests.get(base_url + safe_string)
@@ -47,5 +49,5 @@ def get_metro():
     
 if __name__ == "__main__": 
 
-    get_bus()
-    #get_metro()
+    #get_bus()
+    get_metro()
