3e8691e87048f6af60c6ede97fd13fefe15a9633
BB <me@yomama.com>
2023-07-11 16:11:24 +0300

    -

--- nomadicterrain/transit.py
+++ nomadicterrain/transit.py
@@ -1,4 +1,4 @@
-import urllib.parse, requests
+import urllib.parse, requests, json
 import csv, numpy as np, re, os
 import pandas as pd, json, folium
 from unidecode import unidecode
@@ -46,8 +46,56 @@ def get_metro():
     fout = open(metro,"w")
     fout.write(r.text)
     fout.close()
+
+def plot_bus():
+    d = json.loads(open(bus).read())
+    lines = {}
+    nodes = {}
+    for e in d['elements']:
+        if 'lat' in e:
+            nodes[e['id']] = (e['lat'],e['lon'])
+            
+    for e in d['elements']:
+        if e['type'] == 'relation':
+            if 'name' not in e['tags']: continue           
+            line = [m['ref'] for m in e['members']]
+            lines[e['tags']['name']] = line
+
+    m = folium.Map(location=[41,29], tiles='Stamen Terrain', zoom_start=10)
+    
+    for k in lines:
+        coords = [nodes[m] for m in lines[k] if m in nodes]
+        if len(coords)>0:
+            folium.PolyLine(locations=coords, color="blue",weight=1).add_to(m)
+    
+    m.save("/tmp/out.html")
+    
+def plot_metro():
+    d = json.loads(open(metro).read())
+    lines = {}
+    nodes = {}
+    for e in d['elements']:
+        if 'lat' in e:
+            nodes[e['id']] = (e['lat'],e['lon'])
+            
+    for e in d['elements']:
+        if e['type'] == 'relation':
+            if 'name' not in e['tags']: continue           
+            line = [m['ref'] for m in e['members']]
+            lines[e['tags']['name']] = line
+
+    m = folium.Map(location=[41,29], tiles='Stamen Terrain', zoom_start=10)
+    
+    for k in lines:
+        coords = [nodes[m] for m in lines[k] if m in nodes]
+        if len(coords)>0:
+            folium.PolyLine(locations=coords, color="blue",weight=1).add_to(m)
+    
+    m.save("/tmp/out.html")
     
 if __name__ == "__main__": 
 
     #get_bus()
-    get_metro()
+    #get_metro()
+    #plot_bus()
+    plot_metro()
