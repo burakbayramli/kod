cf5b53a4669cdc564a165bbd46463e5a216a33df
BB <me@yomama.com>
2023-07-29 20:41:05 +0300

    -

--- nomadicterrain/main.py
+++ nomadicterrain/main.py
@@ -155,7 +155,7 @@ def show_travel_map(currlat,currlon,map,fout):
     params = json.loads(data)
 
     clat,clon = params['center']
-    m = folium.Map(location=[clat, clon], zoom_start=10, control_scale=True)
+    m = folium.Map(location=[clat, clon], zoom_start=14, control_scale=True)
     lat,lon=float(currlat),float(currlon)
     folium.Marker([lat,lon], icon=folium.Icon(color="green")).add_to(m)
     
