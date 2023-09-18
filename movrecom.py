f47a39d2043561a00f6c87fe53ce64c174c71713
BB <me@yomama.com>
2023-09-18 12:04:27 +0300

    -

--- movrecom.py
+++ movrecom.py
@@ -16,6 +16,27 @@ import os, sys, re, csv
 #d = "/mnt/3d1ece2f-6539-411b-bac2-589d57201626/home/burak/Downloads/ml-latest"
 d = "/mnt/3d1ece2f-6539-411b-bac2-589d57201626/home/burak/Downloads/ml-25m"
 
+def sim_prep():
+    d = "."
+    fin = d + "/ratings.csv"
+    fout = d + "/ratings-json.csv"
+    curruser = 0
+    row_dict = {}
+    fout = open(fout, "w")
+    with open(fin) as csvfile:   
+        rd = csv.reader(csvfile,delimiter=',')
+        headers = {k: v for v, k in enumerate(next(rd))}
+        for row in rd:
+            if row[headers['userId']] != curruser:
+                fout.write(str(curruser) + "|")
+                fout.write(json.dumps(row_dict))
+                fout.write("\n")
+                fout.flush()
+                curruser = row[headers['userId']]
+                row_dict = {}       
+            row_dict[int(row[headers['movieId']])] = float(row[headers['rating']])
+    fout.close()
+    
 def sim():
     import json, csv, pandas as pd, re
     import sys, numpy as np
