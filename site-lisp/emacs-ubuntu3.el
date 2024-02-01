e08d83a26361e271c3bf9180f3458b40d9336931
BB <me@yomama.com>
2024-02-01 14:11:54 +0300

    -

--- site-lisp/emacs-ubuntu3.el
+++ site-lisp/emacs-ubuntu3.el
@@ -1007,6 +1007,7 @@ This command does not push erased text to kill-ring."
 (find-file-other-window "/home/burak/Documents/tw/en/atw.md")
 (find-file-other-window "/home/burak/Documents/repos")
 (find-file-other-window "/home/burak/Documents/classnotes")
+(find-file-other-window "/home/burak/Documents/classnotes/phy/phy_020_strs_06")
 
 
 (switch-to-buffer "*scratch*")
