cbae96c692142315794fec1e247691f932712c06
BB <me@yomama.com>
2024-09-14 16:37:29 +0300

    -

--- blog.py
+++ blog.py
@@ -6,6 +6,7 @@ head = '''
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/MathJax.js?config=TeX-AMS_HTML-full">
 </script>
 <meta charset='utf-8'>
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 </head>
 <body>
 '''
