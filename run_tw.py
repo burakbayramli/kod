bd69cbd8ac817175c2ff0134d6402e62cc412f63
BB <me@yomama.com>
2024-11-04 08:06:36 +0300

    -

--- run_tw.py
+++ run_tw.py
@@ -1,4 +1,13 @@
 import os, sys
 sys.path.append("/home/burak/Documents/tw/en/mbl/2024")
-import util 
-util.kh_djt_538_polls()
+import util
+
+if sys.argv[1] == "538":
+    util.kh_djt_538_polls()
+if sys.argv[1] == "ukr":
+    map_ukraine_suriyak()
+if sys.argv[1] == "sahel":
+    map_sahel_suriyak()
+if sys.argv[1] == "lebanon":
+    map_isr_suriyak()
+    
