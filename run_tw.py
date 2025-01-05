6896bac36503c307834307aa097757327a87f88c
BB <me@yomama.com>
2025-01-05 15:29:09 +0300

    -

--- run_tw.py
+++ run_tw.py
@@ -1,15 +1,7 @@
 import os, sys
-sys.path.append("/home/burak/Documents/tw/en/mbl/2024")
+sys.path.append("/home/burak/Documents/tw/en/mbl/2025")
 import util
 
-if sys.argv[1] == "538":
-    util.kh_djt_538_polls()
 if sys.argv[1] == "ukr":
     util.map_ukraine_suriyak()
-if sys.argv[1] == "sudan":
-    util.map_sahel_suriyak()
-if sys.argv[1] == "isr":
-    util.map_isr_suriyak()
-if sys.argv[1] == "syria":
-    util.map_syria_suriyak()
     
