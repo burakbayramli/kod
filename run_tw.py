65d5993fc88cecab2fcfbecf7d881024032640c1
BB <me@yomama.com>
2025-01-25 18:08:10 +0300

    -

--- run_tw.py
+++ run_tw.py
@@ -5,6 +5,9 @@ import util
 if sys.argv[1] == "ukr":
     util.map_ukraine_suriyak()
     
+if sys.argv[1] == "isr":
+    util.map_isr_suriyak()
+    
 if sys.argv[1] == "approv":
     util.trump_approval()
     
