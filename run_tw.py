3ca610f9f085c3f6f6aa0fd7f57b4fec4bc4561c
BB <me@yomama.com>
2025-03-10 19:19:20 +0300

    -

--- run_tw.py
+++ run_tw.py
@@ -1,15 +1,6 @@
 import os, sys
 sys.path.append("/home/burak/Documents/tw/en/mbl/2025")
 import util
-
-if sys.argv[1] == "ukr":
-    util.map_ukraine_suriyak()
-    
-if sys.argv[1] == "sudan":
-    util.map_sahel_suriyak()
-    
-if sys.argv[1] == "isr":
-    util.map_isr_suriyak()
     
 if sys.argv[1] == "approv":
     util.trump_approval()
