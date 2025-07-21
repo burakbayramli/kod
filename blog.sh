83b15585406cb8f0f8478f0bacbc8f244916e910
BB <me@yomama.com>
2025-07-21 09:34:52 +0300

    -

--- blog.sh
+++ blog.sh
@@ -1,5 +1,4 @@
-python $HOME/Documents/kod/blog.py $1 > `pwd`/out.html
-#xvkbd -window *Firefox* -text "\Cr" > /dev/null 2>&1
-#xvkbd -window *emacs* -text "\Ce" > /dev/null 2>&1
+/usr/bin/pandoc --template=/home/burak/Documents/classnotes/template.html -M title="test" --mathjax=https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/MathJax.js?config=TeX-AMS_HTML-full -f markdown -t html $1 -o `pwd`/out.html
+#python $HOME/Documents/kod/blog.py $1 > `pwd`/out.html
 wc -l  $1
 sort $1 | uniq -cd | sort -nr
