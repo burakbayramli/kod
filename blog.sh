a8ea9412babe53ac6af9f333083c254260f16329
BB <me@yomama.com>
2025-09-27 08:50:04 +0300

    -

--- blog.sh
+++ blog.sh
@@ -1,5 +1,7 @@
 filename=$1
 filename="${filename%.*}"
 /usr/bin/pandoc --template=/home/burak/Documents/classnotes/template.html -M title="test" --mathjax=https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/MathJax.js?config=TeX-AMS_HTML-full -f markdown -t html $filename.md -o `pwd`/$filename.html
+echo $filename
+perl -pi -e 's/<title>test<\/title>/<title><\/title>/sg'  `pwd`/$filename.html
 wc -l  $filename.md
 sort $filename.md | uniq -cd | sort -nr
