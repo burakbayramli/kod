4e0811ea5ee7ce1c89f60b8024df2a214d38eba7
BB <me@yomama.com>
2026-01-31 18:02:24 +0300

    -

--- aloud.py
+++
@@ -1,8 +0,0 @@
-import gtts, os, sys
-cmd = "pdftotext -f %d -l %d %s /tmp/out.txt"
-cmd = cmd % (int(sys.argv[2]), int(sys.argv[3]), sys.argv[1])
-os.system(cmd)
-content = open("/tmp/out.txt").read()
-tts = gtts.gTTS(text=content, lang="en")
-filename = os.environ['HOME'] + "/Downloads/out-%d-%d.mp3" % (int(sys.argv[2]), int(sys.argv[3]))
-tts.save(filename)
